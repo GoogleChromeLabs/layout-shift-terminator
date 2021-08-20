17c36e2385e048dcc1e46d33dd52cd949d79cfed
Glitch (glitch-blank-website) <none>
2021-08-20 22:34:31 +0000

    👷💫 Checkpoint ./style.css:7427215/118 ./script.js:7427215/1748 ./index.html:7427215/16

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 const viewportSizes = [
   {"width": 1920, "height": 1080},
-  {"width": 1536, "height": 864},
-  {"width": 1366, "height": 768},
-  {"width": 768, "height": 1024},
-  {"width": 414, "height": 896},
-  {"width": 375, "height": 667},
-  {"width": 360, "height": 640}
+  // {"width": 1536, "height": 864},
+  // {"width": 1366, "height": 768},
+  // {"width": 768, "height": 1024},
+  // {"width": 414, "height": 896},
+  // {"width": 375, "height": 667},
+  // {"width": 360, "height": 640}
 ];
 
 document.addEventListener( 'DOMContentLoaded', () => {
@@ -23,11 +23,31 @@ document.addEventListener( 'DOMContentLoaded', () => {
 } );
 
 
-function start() {
+async function start() {
   const calculationContainer = document.getElementById('calculation');
   calculationContainer.hidden = false;
+  const markup = document.getElementById('markup').value;
   
-  const viewportIframe = document.querySelector('iframe');
-  viewportIframe.src = 'about:blank';
-  
+  const progress = document.querySelector('progress');
+  progress.max = viewportSizes.length;
+  let i = 1;
+  for ( const viewportSize of viewportSizes ) {
+    await calculateViewportSize( { markup, ...viewportSize } );
+    
+    
+    
+    progress.value = i;
+    i++;
+  }
+}
+
+
+async function calculateViewportSize( { width, height, markup } ) {
+  const iframe = document.querySelector('iframe');
+  iframe.src = 'about:blank';
+  iframe.width = width;
+  iframe.height = height;
+  iframe.contentWindow.document.open();
+  iframe.contentWindow.document.write(markup);
+  iframe.contentWindow.document.close();
 }
