2555cf0781ef256b611cbc975b739cbd4f69fb65
Glitch (glitch-blank-website) <none>
2021-08-21 05:52:27 +0000

    🚩🎻 Checkpoint ./measurement.js:7427215/67 ./style.css:7427215/112 ./viewports.js:7427215/106

--- viewports.js
+++ viewports.js
@@ -3,6 +3,9 @@ export default [
   { width: 1536, height: 864 },
   { width: 1366, height: 768 },
   { width: 768, height: 1024 },
+  { width: 700, height: 1024 },
+  { width: 600, height: 1024 },
+  { width: 500, height: 1024 },
   { width: 414, height: 896 },
   { width: 375, height: 667 },
   { width: 360, height: 640 }
